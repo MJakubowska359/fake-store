de60c95a6b08f2c15b35543e6f26c434e5a99722
Monika Jakubowska <mjakubowska.official@gmail.com>
2025-10-20 21:31:58 +0200

    refactor (tests): remove unnecessary serial mode and move repeated steps to beforeEach

--- tests/wishlist.spec.ts
+++ tests/wishlist.spec.ts
@@ -5,7 +5,6 @@ import { ShopPage } from '../src/pages/shop.page';
 import { WishlistPage } from '../src/pages/wishlist.page';
 import { expect, test } from '@playwright/test';
 
-test.describe.configure({ mode: 'serial' });
 test.describe('Verify wishlist', () => {
   let shopPage: ShopPage;
   let productCategoryPage: ProductCategoryPage;
@@ -21,32 +20,28 @@ test.describe('Verify wishlist', () => {
     basketPage = new BasketPage(page);
 
     await shopPage.goto();
-  });
-
-  test('Remove a product from the wishlist', async () => {
-    // Arrange
-    const expectedAlertContent = 'Produkt został usunięty.';
-
-    // Act
     await shopPage.clickWindsurfingCategory();
     await productCategoryPage.clickWindsurfingInGreece();
     await productPage.clickAddToWishlistButton();
     await wishlistPage.goto();
+  });
+
+  test('Remove the product from the wishlist', async () => {
+    // Arrange
+    const expectedAlertContent = 'Produkt został usunięty.';
+
+    // Act
     await wishlistPage.deleteProductFromWishlist();
 
     // Assert
     await expect(wishlistPage.alert).toHaveText(expectedAlertContent);
   });
 
-  test('Add a product to the basket from the wishlist', async () => {
+  test('Add the product to the basket from the wishlist', async () => {
     // Arrange
     const expectedHeader = 'Koszyk';
 
     // Act
-    await shopPage.clickClimbingCategory();
-    await productCategoryPage.clickClimbingInIslandPeak();
-    await productPage.clickAddToWishlistButton();
-    await wishlistPage.goto();
     await wishlistPage.addProductFromWishlistToBasket();
 
     // Assert
