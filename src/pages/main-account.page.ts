56f2b5a22595a3506f9c58e4e38a7fc2f3e889c7
Monika Jakubowska <mjakubowska.official@gmail.com>
2025-10-25 17:14:57 +0200

    test: changing password

--- src/pages/main-account.page.ts
+++ src/pages/main-account.page.ts
@@ -1,11 +1,24 @@
+import { RegisterUserModel } from '../models/user.model';
 import { BasePage } from './base.page';
+import { faker } from '@faker-js/faker/locale/en';
 import { Locator, Page } from '@playwright/test';
 
 export class MainAccountPage extends BasePage {
   url = '/moje-konto';
 
   deleteAccountLink: Locator;
+  accountSettingsLink: Locator;
   logoutLink: Locator;
+
+  // First name and Last name
+  firstNameInput: Locator;
+  lastNameInput: Locator;
+
+  // Change password
+  currentlyPasswordInput: Locator;
+  newPasswordInput: Locator;
+  submitPasswordInput: Locator;
+  saveChanges: Locator;
 
   header: Locator;
 
@@ -15,9 +28,22 @@ export class MainAccountPage extends BasePage {
     this.deleteAccountLink = this.page.getByRole('link', {
       name: 'Delete Account',
     });
+    this.accountSettingsLink = this.page.locator('li').getByRole('link', {
+      name: 'Edycja konta',
+    });
     this.logoutLink = this.page.locator('li').getByRole('link', {
       name: 'Wyloguj',
     });
+
+    // First name and las name
+    this.firstNameInput = this.page.locator('#account_first_name');
+    this.lastNameInput = this.page.locator('#account_last_name');
+
+    // Reset password
+    this.currentlyPasswordInput = this.page.locator('#password_current');
+    this.newPasswordInput = this.page.locator('#password_1');
+    this.submitPasswordInput = this.page.locator('#password_2');
+    this.saveChanges = this.page.getByRole('button', { name: 'Zapisz zmiany' });
 
     this.header = this.page.getByRole('heading');
   }
@@ -29,6 +55,23 @@ export class MainAccountPage extends BasePage {
       dialog.accept().catch(() => {});
     });
     await this.deleteAccountLink.click();
+  }
+
+  async clickEditAccount(): Promise<void> {
+    await this.accountSettingsLink.click();
+  }
+
+  async changePassword(registerUserData: RegisterUserModel): Promise<void> {
+    await this.firstNameInput.fill(
+      faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+    );
+    await this.lastNameInput.fill(
+      faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+    );
+    await this.currentlyPasswordInput.fill(registerUserData.userPassword);
+    await this.newPasswordInput.fill('testoweH4sł@');
+    await this.submitPasswordInput.fill('testoweH4sł@');
+    await this.saveChanges.click();
   }
 
   async clickLogoutButton(): Promise<void> {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,8 @@
+import { prepareRandomUser } from '../src/factories/user.factory';
+import { RegisterUserModel } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
+import { MainAccountPage } from '../src/pages/main-account.page';
+import { RegisterPage } from '../src/pages/register.page';
 import {
   incorrect_email,
   incorrect_username,
@@ -9,9 +13,15 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   let loginPage: LoginPage;
+  let registerPage: RegisterPage;
+  let registerUserData: RegisterUserModel;
+  let mainAccount: MainAccountPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
+    registerPage = new RegisterPage(page);
+    registerUserData = prepareRandomUser();
+    mainAccount = new MainAccountPage(page);
 
     await loginPage.goto();
   });
@@ -61,13 +71,26 @@ test.describe('Verify login', () => {
     await expect(loginPage.alert).toHaveText(expectedAlertContent);
   });
 
-  test('Reset password', async () => {
+  test('Reset password from login page', async () => {
     // Arrange
     const expectedNotificationContent = 'Wysłano e-mail do zresetowania hasła.';
 
     // Act
     await loginPage.clickResetPasswordOption();
     await loginPage.resetPassword(testUser1);
+
+    // Assert
+    await expect(loginPage.alert).toHaveText(expectedNotificationContent);
+  });
+
+  test('Reset password from account', async () => {
+    // Arrange
+    const expectedNotificationContent = 'Zmieniono szczegóły konta.';
+
+    // Act
+    await registerPage.register(registerUserData);
+    await mainAccount.clickEditAccount();
+    await mainAccount.changePassword(registerUserData);
 
     // Assert
     await expect(loginPage.alert).toHaveText(expectedNotificationContent);
