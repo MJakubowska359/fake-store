9a780700efa9b0630ac24ccee4d298a0ed775b41
Monika Jakubowska <mjakubowska.official@gmail.com>
2026-05-26 22:25:50 +0200

    test(regression): add search regression scenarios

---
+++ tests/regression/search-engine.spec.ts
@@ -0,0 +1,32 @@
+import { BasePage } from '../../src/pages/base.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify search engine', () => {
+  let basePage: BasePage;
+
+  test.beforeEach(async ({ page }) => {
+    basePage = new BasePage(page);
+
+    await basePage.goto();
+  });
+
+  test('User can search products by price', async () => {
+    // Arrange
+    const expectedHeader = 'Wyniki wyszukiwania: „2 599,00 zł”';
+
+    // Act
+    await basePage.fillPrizeInSearchEngine();
+    await expect(basePage.header).toHaveText(expectedHeader);
+
+    // Assert
+    await expect(basePage.information).toBeVisible();
+  });
+
+  test('User can search products using letters from product names', async () => {
+    // Act
+    await basePage.fillLettersInSearchEngine();
+
+    // Assert
+    await expect(basePage.searchResult).toHaveCount(4);
+  });
+});
