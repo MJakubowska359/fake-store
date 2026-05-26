45afcccae1934918423865f5d19099588a89c4e2
Monika Jakubowska <mjakubowska.official@gmail.com>
2026-05-26 22:19:31 +0200

    test(smoke): move wishlist add-to-basket test to smoke suite

---
+++ tests/smoke/wishlist.spec.ts
@@ -0,0 +1,40 @@
+import { BasketPage } from '../../src/pages/basket.page';
+import { ProductCategoryPage } from '../../src/pages/product-category.page';
+import { ProductPage } from '../../src/pages/product.page';
+import { ShopPage } from '../../src/pages/shop.page';
+import { WishlistPage } from '../../src/pages/wishlist.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify wishlist', () => {
+  let shopPage: ShopPage;
+  let productCategoryPage: ProductCategoryPage;
+  let productPage: ProductPage;
+  let wishlistPage: WishlistPage;
+  let basketPage: BasketPage;
+
+  test.beforeEach(async ({ page }) => {
+    shopPage = new ShopPage(page);
+    productCategoryPage = new ProductCategoryPage(page);
+    productPage = new ProductPage(page);
+    wishlistPage = new WishlistPage(page);
+    basketPage = new BasketPage(page);
+
+    await shopPage.goto();
+    await shopPage.clickWindsurfingCategory();
+    await productCategoryPage.clickWindsurfingInGreece();
+    await productPage.clickAddToWishlistButton();
+    await wishlistPage.goto();
+    await page.reload();
+  });
+
+  test('User can add a product to the basket from the wishlist', async () => {
+    // Arrange
+    const expectedHeader = 'Koszyk';
+
+    // Act
+    await wishlistPage.addProductFromWishlistToBasket();
+
+    // Assert
+    await expect(basketPage.header.first()).toHaveText(expectedHeader);
+  });
+});
