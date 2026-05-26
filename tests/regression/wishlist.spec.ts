9485af71937edb0a1f9458828ec6f531ead0861d
Monika Jakubowska <mjakubowska.official@gmail.com>
2026-05-26 22:16:31 +0200

    test(regression): add two wishlist scenarios to regression suite

--- tests/regression/wishlist.spec.ts
+++ tests/regression/wishlist.spec.ts
@@ -1,23 +1,20 @@
-import { BasketPage } from '../src/pages/basket.page';
-import { ProductCategoryPage } from '../src/pages/product-category.page';
-import { ProductPage } from '../src/pages/product.page';
-import { ShopPage } from '../src/pages/shop.page';
-import { WishlistPage } from '../src/pages/wishlist.page';
+import { ProductCategoryPage } from '../../src/pages/product-category.page';
+import { ProductPage } from '../../src/pages/product.page';
+import { ShopPage } from '../../src/pages/shop.page';
+import { WishlistPage } from '../../src/pages/wishlist.page';
 import { expect, test } from '@playwright/test';
 
-test.describe('Verify wishlist', () => {
+test.describe('Wishlist @regression', () => {
   let shopPage: ShopPage;
   let productCategoryPage: ProductCategoryPage;
   let productPage: ProductPage;
   let wishlistPage: WishlistPage;
-  let basketPage: BasketPage;
 
   test.beforeEach(async ({ page }) => {
     shopPage = new ShopPage(page);
     productCategoryPage = new ProductCategoryPage(page);
     productPage = new ProductPage(page);
     wishlistPage = new WishlistPage(page);
-    basketPage = new BasketPage(page);
 
     await shopPage.goto();
     await shopPage.clickWindsurfingCategory();
@@ -27,7 +24,7 @@ test.describe('Verify wishlist', () => {
     await page.reload();
   });
 
-  test('Remove the product from the wishlist', async () => {
+  test('User can remove a product from the wishlist', async () => {
     // Arrange
     const expectedAlertContent = 'Produkt został usunięty.';
 
@@ -38,18 +35,7 @@ test.describe('Verify wishlist', () => {
     await expect(wishlistPage.alert).toHaveText(expectedAlertContent);
   });
 
-  test('Add the product to the basket from the wishlist', async () => {
-    // Arrange
-    const expectedHeader = 'Koszyk';
-
-    // Act
-    await wishlistPage.addProductFromWishlistToBasket();
-
-    // Assert
-    await expect(basketPage.header.first()).toHaveText(expectedHeader);
-  });
-
-  test('Go back to the product from the wishlist', async () => {
+  test('User can navigate to the product page from the wishlist', async () => {
     // Arrange
     const expectedHeader = 'Grecja – Limnos';
 
